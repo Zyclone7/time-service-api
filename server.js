c273350a899eb565d1fba1372d43a32defbfe79a
Zyclone7 <pillagian7@gmail.com>
2024-12-23 02:11:59 +0800

    Ready to Deploy

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-require('dotenv').config(); // to use environment variables
 const express = require('express');
-const cors = require('cors');
 const morgan = require('morgan');
+require('colors'); // Import colors for terminal coloring
 const connectDB = require('./config/db');
-const timeRoutes = require('./routes/timeSpentRoutes');
+const corsConfig = require('./middleware/corsConfig');
+const timeSpentRoutes = require('./routes/timeSpentRoutes');
+require('dotenv').config(); // Load environment variables from .env
 
 const app = express();
 const PORT = process.env.PORT || 5002;
 
-// Connect to MongoDB
+// Connect to the database
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(corsConfig);
 app.use(express.json());
 app.use(morgan('dev'));
 
 // Routes
-app.use('/time-spent', timeSpentRoutes);
+app.use('/api/time-spent', timeSpentRoutes);
 
-// Start server
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`.cyan); // Output in cyan
 });
